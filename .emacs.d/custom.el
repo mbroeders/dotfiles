71d0a61b8f932d274f585e78d131d9fef842c725
Mark Broeders <inbox@markbroeders.nl>
2021-02-19 15:33:01 +0100

    Updating polybar

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(esup projectile rainbow-delimiters org-gcal org-cal eradio dired-hide-dotfiles all-the-icons-dired vterm which-key visual-fill-column use-package org-bullets no-littering ivy-rich ivy-prescient hydra helpful general exwm evil-collection doom-themes doom-modeline counsel auto-package-update)))
+   '(desktop-environment counsel-projectile pinentry which-key vterm visual-fill-column use-package rainbow-delimiters projectile org-gcal org-bullets no-littering ivy-rich ivy-prescient hydra helpful general exwm evil-collection esup eradio doom-themes doom-modeline dired-hide-dotfiles counsel auto-package-update all-the-icons-dired)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.

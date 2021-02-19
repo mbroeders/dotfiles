178a616cf6f53b01b713a22354eb911683b6ad72
Mark Broeders <inbox@markbroeders.nl>
2021-02-19 14:09:36 +0100

    Cleanup

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(org-gcal org-cal eradio which-key vterm visual-fill-column use-package rainbow-delimiters pinentry org-bullets no-littering ivy-rich ivy-prescient hydra helpful general exwm evil-collection esup doom-themes doom-modeline dired-hide-dotfiles counsel-projectile auto-package-update all-the-icons-dired)))
+   '(esup projectile rainbow-delimiters org-gcal org-cal eradio dired-hide-dotfiles all-the-icons-dired vterm which-key visual-fill-column use-package org-bullets no-littering ivy-rich ivy-prescient hydra helpful general exwm evil-collection doom-themes doom-modeline counsel auto-package-update)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
